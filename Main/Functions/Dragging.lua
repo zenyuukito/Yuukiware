9e38318abc7d74f7721796ba34972f8329a73cdb
zenyuukito <zenyuukito@gmail.com>
2026-05-06 15:41:25 +0100

    Update and rename Dragging to Dragging.lua

---
+++ Main/Functions/Dragging.lua
@@ -0,0 +1,36 @@
+local DragModule = {}
+local UserInputService = game:GetService("UserInputService")
+
+function DragModule.MakeDraggable(frame, handle)
+    local dragToggle, dragStart, startPos
+    handle = handle or frame -- If no handle is provided, the whole frame is the handle
+
+    handle.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+            dragToggle = true
+            dragStart = input.Position
+            startPos = frame.Position
+            
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragToggle = false
+                end
+            end)
+        end
+    end)
+
+    UserInputService.InputChanged:Connect(function(input)
+        if dragToggle and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+            local delta = input.Position - dragStart
+            -- Calculation: New Position = Start Position + Change in Mouse Position
+            frame.Position = UDim2.new(
+                startPos.X.Scale, 
+                startPos.X.Offset + delta.X, 
+                startPos.Y.Scale, 
+                startPos.Y.Offset + delta.Y
+            )
+        end
+    end)
+end
+
+return DragModule
