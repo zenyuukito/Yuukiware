6a2195a6884077ce65d8e7c3ebe6570e8b755b8f
zenyuukito <zenyuukito@gmail.com>
2026-05-07 21:41:55 +0100

    Create Globals.lua

---
+++ Main/Core/Globals.lua
@@ -0,0 +1,11 @@
+-- YUUKIWARE: GLOBALS CACHE
+local Globals = {}
+
+Globals.Players = game:GetService("Players")
+Globals.LP = Globals.Players.LocalPlayer
+Globals.TS = game:GetService("TweenService")
+Globals.UIS = game:GetService("UserInputService")
+Globals.RS = game:GetService("RunService")
+Globals.CG = game:GetService("CoreGui")
+
+return Globals
